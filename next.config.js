a92f2a866d901fcb30285092e127b9258d0f3144
9M2PJU <9m2pju@hamradio.my>
2026-01-18 14:18:22 +0800

    fix: update configurations for tailwind v4, esm, and next.js

--- next.config.js
+++ next.config.js
@@ -3,13 +3,9 @@ const nextConfig = {
     images: {
         unoptimized: true,
     },
-    // Ensure we ignore build errors for now to get it up
     typescript: {
         ignoreBuildErrors: true,
-    },
-    eslint: {
-        ignoreDuringBuilds: true,
     }
 };
 
-module.exports = nextConfig;
+export default nextConfig;
